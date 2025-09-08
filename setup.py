c155d95f99d64a9d27036fd0493a995162568e51
Roberts Slisans <rsxdalv@gmail.com>
2025-09-08 14:30:22 +0300

    init

--- setup.py
+++ setup.py
@@ -6,13 +6,15 @@ setuptools.setup(
     version="0.0.1",
     author="Your Name",
     description="A template extension for TTS Generation WebUI",
-    url="https://github.com/yourusername/extension_kitten_tts",
+    url="https://github.com/rsxdalv/extension_kitten_tts",
     project_urls={},
     scripts=[],
     install_requires=[
         # Add your dependencies here
         # "numpy",
         # "torch",
+        # pip install https://github.com/KittenML/KittenTTS/releases/download/0.1/kittentts-0.1.0-py3-none-any.whl
+        "kittentts @ https://github.com/KittenML/KittenTTS/releases/download/0.1/kittentts-0.1.0-py3-none-any.whl",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
